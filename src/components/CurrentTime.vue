573093199f000a7dc3c966a6befe4679792394da
Pheungnoi CMP <nattakritta.sak@mfu.ac.th>
2019-04-07 14:25:06 +0700

    Add CurrentTime

---
+++ src/components/CurrentTime.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+       <!-- <h3>{{ stationData.value[0].log_datetime }}</h3>!-->
+        <h3>{{ log_datetime }}</h3>
+    </div>
+</template>
+<script>
+
+export default {
+    name:"CurrentTime",
+    props:["log_datetime"] /* เปลี่ยนขื่อนตาม api ส่งมา */
+}
+</script>
