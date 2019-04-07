1aa4895448e1ec81c9eba8145ee92f30a1fbf190
jeabwaraporn <jeab.waraporn@gmail.com>
2019-04-07 15:30:55 +0700

    edit theme

--- src/App.vue
+++ src/App.vue
@@ -28,7 +28,7 @@ body {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  color: #ff6600;
 }
 
 main {
@@ -40,7 +40,7 @@ header {
   margin: 0;
   height: 56px;
   padding: 0 16px 0 24px;
-  background-color: #35495E;
+  background-color: #F08080;
   color: #ffffff;
 }
 
