8a9b686c13304ec7a7d9ec7ca1e10572b77c0f6a
Jirapong Sittilert <jirapong.sit@mfu.ac.th>
2019-04-07 14:35:51 +0700

    add location file

---
+++ src/components/Location.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="Location">
+    <label for=""><h1><img src="../assets/gps.png" alt>{{ stationData.dustboy_name }}</h1></label>
+  </div>
+</template>
+
+<script>
+//import StationApi from "@/api-services/stations.service";
+
+export default {
+  name: 'Location',
+  data () {
+    return {
+      stationData: {}
+    }
+  }
+}
+</script>
